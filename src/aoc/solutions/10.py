9b3b8514a99b816ed799ab41e4e7c53db5050d76
Pär Wieslander <wieslander@gmail.com>
2021-12-10 08:50:43 +0100

    Day 10

---
+++ src/aoc/solutions/10.py
@@ -0,0 +1,78 @@
+"""Advent of Code - 2021 Day 10"""
+
+brace_map = {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+    '<': '>',
+}
+
+score_map = {
+    ')': 3,
+    ']': 57,
+    '}': 1197,
+    '>': 25137,
+}
+
+autocomplete_score_map = {
+    ')': 1,
+    ']': 2,
+    '}': 3,
+    '>': 4,
+}
+
+def is_opening_brace(ch):
+    return ch in brace_map
+
+
+def is_closing_brace(ch):
+    return ch in brace_map.values()
+
+
+def syntax_error_score(line):
+    stack = []
+    for ch in line:
+        if is_opening_brace(ch):
+            stack.append(ch)
+        elif is_closing_brace(ch):
+            last_opening_brace = stack.pop()
+            if ch != brace_map[last_opening_brace]:
+                return score_map[ch]
+        else:
+            raise RuntimeError(f"Invalid character {ch}")
+
+    return 0
+
+
+def completion_score(line):
+    total_score = 0
+    for ch in get_autocompletion(line):
+        total_score *= 5
+        total_score += autocomplete_score_map[ch]
+    return total_score
+
+
+def get_autocompletion(line):
+    stack = []
+    for ch in line:
+        if is_opening_brace(ch):
+            stack.append(ch)
+        elif is_closing_brace(ch):
+            stack.pop()
+
+    completion_chars = []
+    while stack:
+        ch = stack.pop()
+        completion_chars.append(brace_map[ch])
+
+    return completion_chars
+
+
+def part1(input):
+    return sum(syntax_error_score(line) for line in input.lines())
+
+
+def part2(input):
+    incomplete_lines = [l for l in input.lines() if syntax_error_score(l) == 0]
+    scores = sorted(completion_score(l) for l in incomplete_lines)
+    return scores[len(scores) // 2]
